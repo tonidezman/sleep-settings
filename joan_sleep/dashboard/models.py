a7f7159155a4e8f8bfd71ed33c00bf742cc0f2e3
Toni Dezman <toni.dezman@gmail.com>
2018-05-07 11:02:48 +0200

    adds SleepSetting Model and migrations

--- joan_sleep/dashboard/models.py
+++ joan_sleep/dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class SleepSetting(models.Model):
+    # weekdays
+    monday = models.BooleanField()
+    tuesday = models.BooleanField()
+    wednesday = models.BooleanField()
+    thursday = models.BooleanField()
+    friday = models.BooleanField()
+    saturday = models.BooleanField()
+    sunday = models.BooleanField()
+
+    from_hour = models.TimeField()
+    to_hour = models.TimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
