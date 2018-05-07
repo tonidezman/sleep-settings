d1963ec2038d03af47453edb638366d837dada34
Toni Dezman <toni.dezman@gmail.com>
2018-05-07 10:43:50 +0200

    app uses Postgres instead of Sqlite3

--- joan_sleep/joan_sleep/settings.py
+++ joan_sleep/joan_sleep/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'joan_sleep.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
