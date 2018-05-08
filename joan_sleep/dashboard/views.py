c07e52832c39aa79caf4276d89b7d8b99e7832b8
Toni Dezman <toni.dezman@gmail.com>
2018-05-08 13:52:13 +0200

    adds validation for when user does not choose any weekdays

--- joan_sleep/dashboard/views.py
+++ joan_sleep/dashboard/views.py
@@ -11,8 +11,7 @@ class DashboardView(TemplateView):
         self._create_sleep_setting_if_none_present()
         sleep_setting = SleepSetting.objects.filter().values()[0]
         form = SleepSettingsForm(initial=sleep_setting)
-        context = { 'form': form}
-        return render(request, self.template_name, context)
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         self._create_sleep_setting_if_none_present()
@@ -20,10 +19,18 @@ class DashboardView(TemplateView):
         form = SleepSettingsForm(request.POST or None, instance=sleep_setting)
 
         if form.is_valid():
-            form.save()
+            if self._at_least_one_day_chosen(form.cleaned_data):
+                form.save()
             return redirect('dashboard-settings')
         return render(request, self.template_name, {'form': form})
 
     def _create_sleep_setting_if_none_present(self):
         if SleepSetting.objects.count() == 0:
             SleepSetting().save()
+
+    def _at_least_one_day_chosen(self, form_data):
+        days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        for day in days:
+            if form_data.get(day):
+                return True
+        return False
