ec415e0c50240eb8672b6657026585c9d1cc17ff
Toni Dezman <toni.dezman@gmail.com>
2018-05-09 08:49:37 +0200

    removes django rest framework

--- joan_sleep/joan_sleep/settings.py
+++ joan_sleep/joan_sleep/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'dashboard',
     'material',
     'material.frontend',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
