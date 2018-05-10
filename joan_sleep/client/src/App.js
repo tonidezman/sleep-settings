c675a647ce4750a86568799900a805d6a639ca2d
Toni Dezman <toni.dezman@gmail.com>
2018-05-10 14:15:31 +0200

    adds client page to show active/inactive state

--- joan_sleep/client/src/App.js
+++ joan_sleep/client/src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import moment from 'moment';
 
 class App extends Component {
+  deviceState = {
+    isActive: false,
+    active: 'Active',
+    inactive: 'Inactive'
+  };
+  state = {
+    status: 'Loading...',
+    futureTime: '',
+    minutes: ''
+  };
+  path = 'getSleep/';
+  url = 'http://127.0.0.1:8000';
+
+  componentDidMount() {
+    axios.get(`${this.url}/${this.path}`).then(res => {
+      this.setState({ futureTime: res.data.shouldSleep });
+
+      if (res.data.shouldSleep !== false) {
+        const futureTime = this.state.futureTime;
+        const now = moment().subtract({ hours: 2 });
+        const future = moment(futureTime.slice(0, 19)).utc();
+        const duration = future - now;
+        const minutesToGo = Math.floor(duration / 60000);
+        this.setState({
+          isActive: true,
+          status: this.deviceState.inactive,
+          minutes: minutesToGo
+        });
+      } else {
+        this.setState({ status: this.deviceState.active });
+      }
+    });
+  }
+
   render() {
+    let minutes = '';
+    if (this.state.isActive) {
+      minutes = <div>{this.state.minutes} minutes till activation.</div>;
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>Device state: {this.state.status}</div>
+        {minutes}
       </div>
     );
   }
